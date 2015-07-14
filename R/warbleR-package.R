9b0edc535733f32a81a70325e764d85b55cc250e
maRce10 <marceloa27@gmail.com>
2015-07-13 19:26:45 -0600

    add links to functions in description

--- R/warbleR-package.R
+++ R/warbleR-package.R
@@ -19,38 +19,38 @@
 #'   
 #' @section Accessing Xeno Canto:
 #'   
-#'   \strong{querxc()}: Download recordings and metadata from Xeno Canto
+#'  \code{\link{querxc}}: Download recordings and metadata from Xeno Canto
 #'   
-#'   \strong{xcmaps()}: Create maps to visualize the geographic spread of Xeno 
+#'   \code{\link{xcmaps}}: Create maps to visualize the geographic spread of Xeno 
 #'   Canto recordings
 #'   
 #' @section Managing sound files:
 #'   
-#'   \strong{mp32wav()}: Convert several .mp3 files in working directory to .wav
+#'   \code{\link{mp32wav}}: Convert several .mp3 files in working directory to .wav
 #'   format
 #'   
-#'   \strong{checkwavs()}: Check whether .wav files can be read by subsequent 
+#'   \code{\link{checkwavs}}: Check whether .wav files can be read by subsequent 
 #'   functions
 #'   
 #' @section Streamlining acoustic analysis in R:
 #'   
-#'   \strong{manualoc()}: Interactive spectrographic view to measure start and 
+#'   \code{\link{manualoc}}: Interactive spectrographic view to measure start and 
 #'   end of acoustic signals
 #'   
-#'   \strong{lspec()}: Produce spectrograms of whole recordings split into 
+#'   \code{\link{lspec}}: Produce spectrograms of whole recordings split into 
 #'   multiple rows
 #'   
-#'   \strong{specreator()}: Create spectrograms of manualoc selections
+#'   \code{\link{specreator}}: Create spectrograms of manualoc selections
 #'   
-#'   \strong{snrspecs()}: Create spectrograms to visualize margins over which 
+#'   \code{\link{snrspecs}}: Create spectrograms to visualize margins over which 
 #'   noise will be measured by sig2noise
 #'   
-#'   \strong{sig2noise()}: Measure signal to noise ratio across multiple files
+#'   \code{\link{sig2noise}}: Measure signal to noise ratio across multiple files
 #'   
-#'   \strong{trackfreqs()}: Create spectrograms to visualize frequency 
+#'   \code{\link{trackfreqs}}: Create spectrograms to visualize frequency 
 #'   measurements
 #'   
-#'   \strong{specan()}: Measures acoustic parameters on selected acoustic 
+#'   \code{\link{specan}}: Measures acoustic parameters on selected acoustic 
 #'   signals
 #'   
 #' @importFrom rjson fromJSON
