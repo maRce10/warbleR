05930c17c1919bcee3b735288c3e916dbaecfb0f
gsmithvi <gsmithvi@gmail.com>
2015-07-26 12:37:46 -0600

    Updated xcmaps documentation

--- R/xcmaps.R
+++ R/xcmaps.R
@@ -3,13 +3,13 @@
 #' \code{xcmaps} creates maps to visualize the geographic spread of Xeno-Canto
 #'   recordings.
 #' @usage xcmaps(X, img = TRUE, it = "jpeg")   
-#' @param X Data frame output from0 \code{\link{querxc}}.
+#' @param X Data frame output from \code{\link{querxc}}.
 #' @param img A logical argument specifying whether an image file of each species
 #'   map should be returned, default is \code{TRUE}.
 #' @param it A character vector of length 1 giving the image type to be used. Currently only
 #' "tiff" and "jpeg" are admitted. Default is "jpeg".
 #' @return A map of Xeno-Canto recordings per species (image file), or a faceted
-#'   R plot of species map(s).
+#'   plot of species map(s) in RStudio.
 #' @export
 #' @name xcmaps
 #' @examples
