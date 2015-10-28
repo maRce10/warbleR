ac1edb729b616143580e4d80c9c04dffe902f182
Grace Smith Vidaurre <gsmithvi@gmail.com>
2015-10-28 16:51:19 -0600

    Added simulated coordinated singing events data as sim.coor.sing into /data, as well as corresponding file with roxygen code for documentation in /R. Two grammatical corrections to roxygen documentation of coor.test.

---
+++ R/sim.coor.sing-data.R
@@ -0,0 +1,12 @@
+#' Simulated coordinated singing events.
+#' 
+#' @format \describe{
+#'   
+#'   \item{sim.coor.sing}{Simulated coordinated singing events that overlap and do not overlap most of the time,
+#'   for use with \code{coor.test}}
+#'   
+#'   }
+#'   
+#' @usage data(sim.coor.sing)
+#' 
+"sim.coor.sing" 
