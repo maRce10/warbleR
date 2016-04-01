00c7d8984cd4cf55ef0253b158de2defdc1f472c
maRce10 <marceloa27@gmail.com>
2016-03-31 23:36:18 -0600

    fix documentation

--- R/compare.methods.R
+++ R/compare.methods.R
@@ -3,15 +3,17 @@
 #' \code{compare.methods} Produces graphs with spectrograms from 4 selections that 
 #' allow to visually assess the performance of 2 acoustic distance methods at 
 #' comparing those selections 
-#' @usage compare.methods(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ovlp = 90, 
+#' @usage compare.methods(X = NULL, flim = c(0, 22), bp = c(0, 22), mar = 0.1, wl = 512, ovlp = 90, 
 #' res = 150, n = 10, length.out = 30, methods = c("XCORR", 
 #' "dfDTW", "ffDTW", "SP"),it = "jpeg", parallel = 1)
 #' @param X Data frame with results from \code{\link{manualoc}} function, \code{\link{autodetec}} 
 #' function, or any data frame with columns for sound file name (sound.files), 
 #' selection number (selec), and start and end time of signal (start and end).
 #' Default \code{NULL}. 
-#' @param frange A numeric vector of length 2 for the frequency limit in kHz of 
+#' @param flim A numeric vector of length 2 for the frequency limit in kHz of 
 #'   the spectrogram, as in \code{\link[seewave]{spectro}}. Default is c(0, 22).
+#' @param bp numeric vector of length 2 giving the lower and upper limits of the 
+#' frequency bandpass filter (in kHz) used in the acoustic distance methods. Default is c(0, 22).
 #' @param mar Numeric vector of length 1. Specifies plot margins around selection in seconds. 
 #' @param wl A numeric vector of length 1 specifying the window length of the spectrogram, default 
 #'   is 512.
@@ -38,7 +40,7 @@
 #' of the acoustic space of all signals in the input data frame 'X'.  
 #' @export
 #' @name compare.methods
-#' @details This function produces graphs with spectrograms from 4 selections that allow visual inspection of the performance of acoustic distance methods at comparing those selections. The function compares 2 methods at the time. The methods available are: cross
+#' @details This function produces graphs with spectrograms from 4 selections that allow visual inspection of the performance of acoustic distance methods at comparing those selections. The spectrograms are all show in the same frequency and time scales. The function compares 2 methods at the time. The methods available are: cross
 #' -correlation (XCORR, from \code{xcorr}), dynamic time warping on dominant frequency time 
 #' series (dfDTW, from \code{\link[dtw]{dtw}} applied on \code{dfts} output), dynamic time 
 #' warping on dominant frequency time series (ffDTW, from \code{\link[dtw]{dtw}} applied on 
@@ -50,10 +52,10 @@
 #' to the arrows linking them. The font color of a distance value correspond to the font 
 #' color of the method that generetad it, as shown in the scatterplots. Distances are 
 #' standardize, being 0 the distance of a signal to itself and 1 the farthest pairwise 
-#' distance in the pool of signals. Principal Component Analysis is used to 
-#' calculate distances when using spectral parameters (SP). Classical 
-#' Multidimensional Scalling (also nwon as Principal Coordinates Analysis) 
-#' is used for all other methods. The graphs are return as image files in the 
+#' distance in the pool of signals. Principal Component Analysis (\code{\link[stats]{princomp}}) 
+#' is applied to calculate distances when using spectral parameters (SP). In that case the first 2 PC's are used. Classical 
+#' Multidimensional Scalling (also nwon as Principal Coordinates Analysis, 
+#' (\code{\link[stats]{cmdscale}})) is used for all other methods.Note that SP can only be used with at least 22 selections (number of rows in input data frame) as PCA can only be used with more units than variables. The graphs are return as image files in the 
 #' working directory. The file name contains the methods being compared and the 
 #' rownumber of the selections. This function uses internally a modified version
 #' of the \code{\link[seewave]{spectro}} function from seewave package to create spectrograms. 
@@ -69,7 +71,7 @@
 #' writeWave(Phae.long3,"Phae.long3.wav")s
 #' writeWave(Phae.long4,"Phae.long4.wav") 
 #'
-#' compare.methods(manualoc.df, frange = c(0, 10), mar = 0.1, wl = 512, 
+#' compare.methods(manualoc.df, flim = c(0, 10), bp = c(0, 10), mar = 0.1, wl = 512, 
 #' ovlp = 90, res = 200, n = 10, length.out = 30, 
 #' methods = c("XCORR", "dfDTW"),parallel = 1, it = "tiff")
 #' 
@@ -81,7 +83,7 @@
 #' internally a modified version of the \code{\link[seewave]{spectro}} function from 
 #' seewave package to create spectrograms.
 
-compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ovlp = 90, 
+compare.methods <- function(X = NULL, flim = c(0, 22), bp = c(0, 22), mar = 0.1, wl = 512, ovlp = 90, 
     res = 150, n = 10, length.out = 30, methods = c("XCORR",
 "dfDTW", "ffDTW", "SP"), 
     it = "jpeg", parallel = 1){  
@@ -89,15 +91,21 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   #if parallel is not numeric
   if(!is.numeric(parallel)) stop("'parallel' must be a numeric vector of length 1") 
   if(any(!(parallel %% 1 == 0),parallel < 1)) stop("'parallel' should be a positive integer")
+  
   # methods 
   if(any(!is.character(methods),length(methods) > 2)) stop("'methods' must be a character vector of length 2")
   if(length(methods[!methods %in% c("XCORR", "dfDTW", "ffDTW", "SP")]) > 0) 
     stop(paste(methods[!methods %in% c("XCORR", "dfDTW", "ffDTW", "SP")],"is (are) not valid method"))
   
+  #if flim is not vector or length!=2 stop
+  if(!is.null(flim))
+  {if(!is.vector(flim)) stop("'flim' must be a numeric vector of length 2") else{
+    if(!length(flim) == 2) stop("'flim' must be a numeric vector of length 2")}}    
+ 
   #if bp is not vector or length!=2 stop
-  if(!is.null(frange))
-  {if(!is.vector(frange)) stop("'bp' must be a numeric vector of length 2") else{
-    if(!length(frange) == 2) stop("'bp' must be a numeric vector of length 2")}}    
+  if(!is.null(bp))
+  {if(!is.vector(bp)) stop("'bp' must be a numeric vector of length 2") else{
+    if(!length(bp) == 2) stop("'bp' must be a numeric vector of length 2")}} 
   
   #if wl is not vector or length!=1 stop
   if(is.null(wl)) stop("'wl' must be a numeric vector of length 1") else {
@@ -142,7 +150,7 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   disim.mats <- list()
   
   if("XCORR" %in% methods)
-  {xcmat <- xcorr(X, wl = 512, frange = frange, ovlp = ovlp, dens = 0.9, parallel = parallel)$max.xcorr.matrix
+  {xcmat <- xcorr(X, wl = 512, frange = bp, ovlp = ovlp, dens = 0.9, parallel = parallel)$max.xcorr.matrix
 
   MDSxcorr <- stats::cmdscale(1-xcmat)  
   MDSxcorr <- scale(MDSxcorr)
@@ -150,7 +158,7 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   }
   
   if("dfDTW" %in% methods)
-    {dtwmat <- dfts(X, wl = 512, flim = frange, ovlp = 90, img = FALSE, parallel = parallel, length.out = length.out)
+    {dtwmat <- dfts(X, wl = 512, flim = flim, ovlp = 90, img = FALSE, parallel = parallel, length.out = length.out)
     
   dm <- dtwDist(dtwmat[,3:ncol(dtwmat)],dtwmat[,3:ncol(dtwmat)])  
   
@@ -160,7 +168,7 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   }
 
   if("ffDTW" %in% methods)
-  {dtwmat <- ffts(X, wl = 512, flim = frange, ovlp = 90, img = FALSE, parallel = parallel, length.out = length.out)
+  {dtwmat <- ffts(X, wl = 512, flim = flim, ovlp = 90, img = FALSE, parallel = parallel, length.out = length.out)
   
   dm <- dtwDist(dtwmat[,3:ncol(dtwmat)],dtwmat[,3:ncol(dtwmat)],method="DTW")  
   
@@ -170,7 +178,7 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   }
   
   if("SP" %in% methods)
-  {spmat <- specan(X, wl = 512, bp = frange, parallel = parallel)
+  {spmat <- specan(X, wl = 512, bp = flim, parallel = parallel)
   
   sp <- princomp(scale(spmat[,3:ncol(spmat)]), cor = F)$scores[ ,1:2]
 
@@ -208,21 +216,23 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   options(warn = 0)
 
   #create matrix for sppliting screen
-  m <- rbind(c(0, 2.5/7, 3/9, 5/9), #1
-             c(4.5/7, 1, 3/9, 5/9), #2
-             c(0, 2.5/7, 0, 2/9), #3
-             c(4.5/7, 1, 0, 2/9), #4
-             c(0, 1/2, 5/9, 1), #5
-             c(1/2, 1, 5/9, 1), #6
-             c(0, 2.5/7, 2/9, 3/9), #7 
-             c(2.5/7, 4.5/7, 0, 5/9), #8
-             c(4.5/7, 1, 2/9, 3/9)) #9
+  m <- rbind(c(0, 2.5/7, 3/10, 5/10), #1
+             c(4.5/7, 1, 3/10, 5/10), #2
+             c(0, 2.5/7, 0, 2/10), #3
+             c(4.5/7, 1, 0, 2/10), #4
+             c(0, 1/2, 5/10, 9/10), #5
+             c(1/2, 1, 5/10, 9/10), #6
+             c(0, 2.5/7, 2/10, 3/10), #7 
+             c(2.5/7, 4.5/7, 0, 5/10), #8
+             c(4.5/7, 1, 2/10, 3/10), #9
+             c(0, 3.5/7, 9/10, 10/10), #10
+             c(3.5/7, 1, 9/10, 10/10)) #11
      
   options(warn = -1)
   
   if(parallel == 1)  message("Saving graphs in image files")
   
-  lapp(1:ncol(combs), function(u)
+  invisible(lapp(1:ncol(combs), function(u)
     {
     rs <- combs[,u]
        X <- X[rs,]
@@ -230,8 +240,9 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
 
   # screen 1:4 for spectros
   # screen 5,6 for scatterplots
-  # screen 7:9 for similarities/lines
-  
+  # screen 7:9 for similarities/arrows
+  # screen 10:11 method labels
+       
   if(it == "tiff") tiff(filename = paste("comp.spec-", names(disim.mats)[1],"-",names(disim.mats)[2], paste(X$labels, collapse = "-"), ".tiff", sep = ""), width = 16.25, height =  16.25, units = "cm", res = res) else 
     jpeg(filename = paste("comp.spec-", names(disim.mats)[1],"-",names(disim.mats)[2], paste(X$labels, collapse = "-"), ".jpeg", sep = ""), width =  16.25, height =  16.25, units = "cm", res = res)
   
@@ -243,7 +254,7 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
   
   col[rs] <- topo.colors(5)[1:4]
   
-  invisible(lapply(c(7:9, 1:4, 5:6), function(x)
+  invisible(lapply(c(7:9, 1:4, 5:6, 10:11), function(x)
   {
     screen(x)
     par( mar = rep(0, 4))
@@ -263,15 +274,16 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
       if (tlim[2] > r$samples/r$sample.rate) { tlim[1] <- tlim[1] - r$samples/r$sample.rate - tlim[2]
       tlim[2] <- length(r@left)/r@samp.rate}
       
-      if (frange[2] > ceiling(r$sample.rate/2000) - 1) frange[2] <- ceiling(r$sample.rate/2000) - 1
+      if (flim[2] > ceiling(r$sample.rate/2000) - 1) flim[2] <- ceiling(r$sample.rate/2000) - 1
+      
       
       r <- tuneR::readWave(as.character(X$sound.files[x]), from = tlim[1], to = tlim[2], units = "seconds")
       
-      spectro2(wave = r, f = r@samp.rate,flim = frange, wl = wl, ovlp = ovlp, axisX = F, axisY = F, tlab = F, flab = F, palette = reverse.gray.colors.2)
+      spectro2(wave = r, f = r@samp.rate,flim = flim, wl = wl, ovlp = ovlp, axisX = F, axisY = F, tlab = F, flab = F, palette = reverse.gray.colors.2)
       box(lwd = 2)
       if(x == 1 | x == 3) 
-        text((tlim[2] - tlim[1])*0.95,(frange[2] - frange[1])*0.95, labels = X$labels[x], col = col[rs[x]], cex = 1.5, font = 2) else 
-          text((tlim[2] - tlim[1])*0.05,(frange[2] - frange[1])*0.95, labels = X$labels[x], col = col[rs[x]], cex = 1.5, font = 2)  
+        text((tlim[2] - tlim[1]), (flim[2] - flim[1])*0.86, labels = X$labels[x], col = col[rs[x]], cex = 1.5, font = 2, pos = 2) else 
+          text(0, (flim[2] - flim[1])*0.86, labels = X$labels[x], col = col[rs[x]], cex = 1.5, font = 2, pos = 4)  
 
       abline(v=c(mar1, mar2),lty = 4)
     }
@@ -279,13 +291,12 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
     #upper left
     if(x == 5) {
       plot(disim.mats[[1]], col = "white", xaxt = "n", yaxt = "n", xlim = c(min(disim.mats[[1]][,1]) * 1.1, max(disim.mats[[1]][,1]) * 1.1), ylim = c(min(disim.mats[[1]][,2]) * 1.1, max(disim.mats[[1]][,2]) * 1.1))
-      box( lwd = 4)
+      box(lwd = 4)
       centro <- apply(disim.mats[[1]], 2, mean)
       points(centro[1], centro[2], pch = 20, cex = 2, col = "gray3")
       cex <- rep(1, nrow(disim.mats[[1]]))
       cex[rs] <- 1.4
       text(disim.mats[[1]],  labels = 1:nrow(disim.mats[[1]]), col = col, cex =cex, font = 2)
-      text(min(disim.mats[[1]][,1]) * 0.96, max(disim.mats[[1]][,2]), labels = names(disim.mats)[1], col = 'black', font = 2, cex = 1.2)
     }
     
     #upper right
@@ -297,53 +308,74 @@ compare.methods <- function(X = NULL, frange = c(0, 22), mar = 0.1, wl = 512, ov
       cex <- rep(1, nrow(disim.mats[[2]]))
       cex[rs] <- 1.4
       text(disim.mats[[2]],  labels = 1:nrow(disim.mats[[2]]), col = col, cex =cex, font = 2)
-      text(min(disim.mats[[2]][,1]) * 0.96, max(disim.mats[[2]][,2]), labels = names(disim.mats)[2], col = 'gray', font = 2, cex = 1.2)
     }  
     
     #lower mid
     if(x == 8){
       plot(0.5, xlim = c(0,1), ylim = c(0,1), type = "n", axes = F, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+      lim <- par("usr")
+      rect(lim[1], lim[3]-1, lim[2], lim[4]+1, border = "#FFFFCC", col = "#FFFFCC")
       arrows(0, 5.5/7, 1, 5.5/7, code = 3, length = 0.09, lwd = 2)
-      text(0.5, 5.7/7,labels =round(dist(disim.mats[[1]][rs[c(1,2)],])/maxdist[[1]],2), col = "black", font = 2)
-      text(0.5, 5.3/7,labels =round(dist(disim.mats[[2]][rs[c(1,2)],])/maxdist[[2]],2), col = "gray50", font = 2)
+      text(0.5, 5.36/7,labels =round(dist(disim.mats[[1]][rs[c(1,2)],])/maxdist[[1]],2), col = "black", font = 2, pos = 3)
+      text(0.5, 5.545/7,labels =round(dist(disim.mats[[2]][rs[c(1,2)],])/maxdist[[2]],2), col = "gray50", font = 2, pos = 1)
       arrows(0, 1.5/7, 1, 1.5/7, code = 3, length = 0.09, lwd = 2)
-      text(0.5, 1.7/7,labels = round(dist(disim.mats[[1]][rs[c(3,4)],])/maxdist[[1]],2), col = "black", font = 2)
-      text(0.5, 1.3/7,labels =round(dist(disim.mats[[2]][rs[c(3,4)],])/maxdist[[2]],2), col = "gray50", font = 2)
+      text(0.5, 1.4/7,labels = round(dist(disim.mats[[1]][rs[c(3,4)],])/maxdist[[1]],2), col = "black", font = 2, pos = 3)
+      text(0.5, 1.63/7,labels =round(dist(disim.mats[[2]][rs[c(3,4)],])/maxdist[[2]],2), col = "gray50", font = 2, pos = 1)
       arrows(0, 2/7, 1, 5/7, code = 3, length = 0.09, lwd = 2)
-      text(0.75, 4.7/7,labels =round(dist(disim.mats[[1]][rs[c(2,3)],])/maxdist[[1]],2), col = "black", font = 2)
-      text(0.85, 4.2/7,labels =round(dist(disim.mats[[2]][rs[c(2,3)],])/maxdist[[2]],2), col = "gray50", font = 2)
+      text(0.69, 4.16/7,labels =round(dist(disim.mats[[1]][rs[c(2,3)],])/maxdist[[1]],2), col = "black", font = 2, pos = 3)
+      text(0.85, 4.4/7,labels =round(dist(disim.mats[[2]][rs[c(2,3)],])/maxdist[[2]],2), col = "gray50", font = 2, pos = 1)
       arrows(0, 5/7, 1, 2/7, code = 3, length = 0.09, lwd = 2)
-      text(0.25, 4.7/7,labels =round(dist(disim.mats[[1]][rs[c(1,4)],])/maxdist[[1]],2), col = "black", font = 2)
-      text(0.15, 4.2/7,labels =round(dist(disim.mats[[2]][rs[c(1,4)],])/maxdist[[2]],2), col = "gray50", font = 2)  
+      text(0.3, 4.16/7,labels =round(dist(disim.mats[[1]][rs[c(1,4)],])/maxdist[[1]],2), col = "black", font = 2, pos = 3)
+      text(0.15, 4.4/7,labels =round(dist(disim.mats[[2]][rs[c(1,4)],])/maxdist[[2]],2), col = "gray50", font = 2, pos = 1)  
     }
     
     #in between left
     if(x == 7){
       plot(0.5, xlim = c(0,1), ylim = c(0,1), type = "n", axes = F, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+      lim <- par("usr")
+      rect(lim[1], lim[3]-1, lim[2], lim[4]+1, border = "#FFFFCC", col = "#FFFFCC")
       arrows(0.5, 0, 0.5, 1, code = 3, length = 0.09, lwd = 2)
-      text(0.43, 0.5,labels =round(dist(disim.mats[[1]][rs[c(1,3)],])/maxdist[[1]],2), col = "black", font = 2)
-      text(0.57, 0.5,labels =round(dist(disim.mats[[2]][rs[c(1,3)],])/maxdist[[2]],2), col = "gray50", font = 2)
+      text(0.53, 0.5, labels =round(dist(disim.mats[[1]][rs[c(1,3)],])/maxdist[[1]],2), col = "black", font = 2, pos = 2)
+      text(0.47, 0.5, labels =round(dist(disim.mats[[2]][rs[c(1,3)],])/maxdist[[2]],2), col = "gray50", font = 2, pos = 4)
     }
     
     #in between right
     if(x == 9){
       plot(0.5, xlim = c(0,1), ylim = c(0,1), type = "n", axes = F, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+      lim <- par("usr")
+      rect(lim[1], lim[3]-1, lim[2], lim[4]+1, border = "#FFFFCC", col = "#FFFFCC")
       arrows(0.5, 0, 0.5, 1, code = 3, length = 0.09, lwd = 2)
-      text(0.43, 0.5,labels =round(dist(disim.mats[[1]][rs[c(2,4)],])/maxdist[[1]],2), col = "black", font = 2)
-      text(0.57, 0.5,labels =round(dist(disim.mats[[2]][rs[c(2,4)],])/maxdist[[2]],2), col = "gray50", font = 2)
+      text(0.53, 0.5,labels =round(dist(disim.mats[[1]][rs[c(2,4)],])/maxdist[[1]],2), col = "black", font = 2, pos = 2)
+      text(0.47, 0.5,labels =round(dist(disim.mats[[2]][rs[c(2,4)],])/maxdist[[2]],2), col = "gray50", font = 2, pos = 4)
       
     }
     
+    #top (for method labels)
+    if(x == 10){
+      plot(0.5, xlim = c(0,1), ylim = c(0,1), type = "n", axes = F, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+      lim <- par("usr")
+      rect(lim[1], lim[3]-1, lim[2], lim[4]+1, border = "black", col = "#CCFFCC")
+        text(0.5, 0.5, labels = names(disim.mats)[1], col = 'black', font = 2, cex = 1.2)
+        box(lwd = 4)
+        }
+    
+    if(x == 11){
+      plot(0.5, xlim = c(0,1), ylim = c(0,1), type = "n", axes = F, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+      lim <- par("usr")
+      rect(lim[1], lim[3]-1, lim[2], lim[4]+1, border = "black", col = "#CCFFCC")
+      text(0.5, 0.5, labels = names(disim.mats)[2], col = 'gray50', font = 2, cex = 1.2)      
+      box(lwd = 4)
+    }
   }
   ))
   invisible(dev.off())
   on.exit(invisible(close.screen(all.screens = TRUE)))
-  })
-return()
+  }))
+
 }
 
 
-#internal warbleR function called by compare.methods. Modified from the spectro function from seewave
+#internal warbleR function called by compare.methods. Modified from \code{\link[seewave]{spectro}}  
 spectro2 <-function(wave, f, wl = 512, wn = "hanning", zp = 0, ovlp = 0, 
                     complex = FALSE, norm = TRUE, fftw = FALSE, dB = "max0", 
                     dBref = NULL, plot = TRUE, grid = TRUE, 
@@ -432,9 +464,6 @@ spectro2 <-function(wave, f, wl = 512, wn = "hanning", zp = 0, ovlp = 0,
   }
   Zlim <- range(Z, finite = TRUE, na.rm = TRUE)
   
-  # par(las = 1, col = colaxis, col.axis = colaxis, 
-  # col.lab = collab, bg = colbg, cex.axis = cexaxis, 
-  # cex.lab = cexlab)
   filled.contour.modif2(x = X, y = Y, z = Z, levels = collevels, 
                         nlevels = 20, plot.title = title(main = main, 
                                                          xlab = tlab, ylab = flab), color.palette = palette, 
