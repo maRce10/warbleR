45e66a6b010fa8813f22dd8cb93e3562f2ff7e8c
maRce10 <marceloa27@gmail.com>
2018-05-17 15:20:46 -0400

    S3 generic methods for ext. sel. tab

---
+++ R/zzzz.R
@@ -0,0 +1,31 @@
+
+  make.selection.table <- selection_table
+  auto_detec <- autodetec
+  check_wavs <- checkwavs
+  compare_methods <- compare.methods
+  coor_test <- coor.test
+  coor_graph <- coor.graph
+  df_DTW <- dfDTW
+  df_ts <- dfts
+  ff_DTW <- ffDTW
+  ff_ts <- ffts
+  filter_sels <- filtersels
+  fix_wavs <- fixwavs
+  freq_range <- frange
+  freq_range_detec <- frange.detec
+  manual_loc <- manualoc
+  move_imgs <- move.imgs
+  sel_tailor <- seltailor
+  se_ts <- sp.en.ts
+  snr_specs <- snrspecs
+  spec_an <- specan
+  spectrograms <- specreator
+  track_freqs <- trackfreqs
+  color_spectro <- color.spectro
+  xc_maps <- xcmaps
+  quer_xc <- querxc
+  check_sels <- checksels
+  
+  
+    name_changes <- data.frame('New names' = c("auto_detec", "check_sels", "check_wavs", "compare_methods", "coor_test", "coor_graph", "df_DTW", "df_ts", "ff_ts", "filter_sels", "fix_wavs", "freq_range", "freq_range_detec", "manual_loc", "move_imgs", "sel_tailor","se_ts", "snr_specs", "spec_an", "spectrograms", "track_freqs", "color_spectro", "xc_maps", "quer_xc"),
+                               'Old names'= c("autodetec", "checksels", "checkwavs", "compare.methods", "coor.test", "coor.graph", "dfDTW", "dfts", "ffts", "filtersels", "fixwavs", "frange", "frange.detec", "manualoc", "move.imgs", "seltailor", "sp.en.ts", "snrspecs", "specan", "specreator", "trackfreqs", "color.spectro", "xcmaps", "querxc"))
