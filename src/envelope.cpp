9d71e57c8736e1626e4ed7d0f675b20612a118f7
maRce10 <marceloa27@gmail.com>
2020-11-18 08:06:27 -0600

    Rcpp envelope added

---
+++ src/envelope.cpp
@@ -0,0 +1,98 @@
+#include <Rcpp.h>
+#include <math.h>
+using namespace Rcpp;
+
+//' @title Calculates the absolute amplitude envelope
+//' @usage envelope(wave, ssmooth = 0)
+//' @param wave Numeric vector with amplitude values. Required.
+//' @param ssmooth Numeric vector of length 1 indicating the size of the sliding window use to smooth envelopes. Default is 0 (no smoothing).
+//' @return An amplitude envelope.
+//' @export
+//' @name envelope
+//' @details The function calculates the absolute amplitude envelope of an amplitude vector using compiled C code which is usually several times faster.
+//' @seealso \code{\link[seewave]{env}}.  
+//' @examples
+//' \dontrun{
+//' data(tico)
+//' 
+//' amp_env <- envelope(tico@left, ssmooth = 100)
+//' }
+//' @references {
+//' Araya-Salas, M., & Smith-Vidaurre, G. (2017). warbleR: An R package to streamline analysis of animal acoustic signals. Methods in Ecology and Evolution, 8(2), 184-191.
+//' }
+//' @author Marcelo Araya-Salas (\email{marcelo.araya@@ucr.ac.cr}) & Paula Monge
+// last modification on nov-17-2020 (MAS)
+
+
+double media (NumericVector amp_v,int indice, int length){ 
+  double suma = 0.0, med = 0.0; 
+  double cont = 0.0;
+  
+  for (int index = indice; index <= length; index++){
+    suma += amp_v[index]; 
+    cont++;
+  }
+  
+  med = suma / cont; 
+  return med; 
+}
+
+// [[Rcpp::export]]
+NumericVector envelope(NumericVector wave, int ssmooth = 0){
+    //Saca el tama;o del vector
+    int tam = wave.size();
+    NumericVector abs_amp_v(tam);
+    
+    //Saca el valor absoluto del vector
+    for (int index = 0; index < tam; index++){
+      abs_amp_v[index] = abs(wave[index]);
+    }
+    
+    double half_ssmooth = ssmooth / 2.0;
+    
+    //smoothing
+    if (ssmooth != 0){
+      
+      //crear vector nulo
+      NumericVector  smooth_abs_amp_v(tam);
+    
+      for (int index = 0; index < tam; index++){
+        smooth_abs_amp_v[index] = 0;
+      }
+      
+      //Inicio del vecindario
+      int strt = 0;
+      
+      for(int index = 0; index < tam; index++){
+        
+        if(index+1 - abs(ceil(half_ssmooth)) > 0){
+            strt= index;
+        }
+        else{
+          if(index+1 - abs(ceil(half_ssmooth)) == 0){
+            strt = 0;
+          }
+          else{
+            strt = abs(index+2 - ceil(half_ssmooth));
+          }
+        }
+        
+        //Final del vecindario
+        int end = 0;
+        
+        if (ceil(abs(index+1 + (half_ssmooth))) < tam){
+          end = ceil(abs(index + (half_ssmooth)));
+        }
+        else{
+          end = tam-1;
+        }
+        
+        smooth_abs_amp_v[index+1] = media(abs_amp_v, strt, end);
+      }
+      
+      return smooth_abs_amp_v;
+    }
+    else{
+     return abs_amp_v;
+    }
+}
